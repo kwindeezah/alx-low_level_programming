a7779bde4d87a6f829e0032178cd04b535b02333
kwindeezah <hadizathmotunrayo@gmail.com>
2022-09-08 13:56:23 -0700

    A C program that prints a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -function returns a string
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
