d4b3d818cce031efc300359a1c818d1ce4ccd93f
kwindeezah <hadizathmotunrayo@gmail.com>
2022-09-09 16:04:27 -0700

    Using the for loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		if (n == 9)
+		if (n != 57)
 			continue;
 		putchar(',');
 		putchar(' ');
