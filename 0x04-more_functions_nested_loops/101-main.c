322866cdb32306505de6a9775d13fc17f57d6328
kwindeezah <hadizathmotunrayo@gmail.com>
2022-09-15 18:32:02 -0700

    Commit

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,14 +8,14 @@
 int main(void)
 {
     print_number(98);
-    putchar('\n');
+    _putchar('\n');
     print_number(402);
-    putchar('\n');
+    _putchar('\n');
     print_number(1024);
-    putchar('\n');
+    _putchar('\n');
     print_number(0);
-    putchar('\n');
+    _putchar('\n');
     print_number(-98);
-    putchar('\n');
+    _putchar('\n');
     return (0);
 }
